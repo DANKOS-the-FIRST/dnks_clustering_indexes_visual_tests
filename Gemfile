04b11e0245d9af183c839f7164eb3f6b002bec19
danylokosenko <danylo.kosenko@yalantis.net>
2022-06-13 16:15:04 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "dnks_clustering_indexes"
+gem "gruff"
+gem 'kmeans-clusterer'
+gem "test-unit"
---
+++ main_test.rb
@@ -0,0 +1,41 @@
+require "test/unit"
+require "bigdecimal"
+require "index_calculator"
+require "gruff"
+class CalculationsTest < Test::Unit::TestCase
+  def setup
+  end
+  def test_one_dataset_from_test_file
+    labels = %w[x y]
+    calc = IndexCalculator.new([], labels, 7)
+    filename = "#{Dir.pwd}/test_data/5.txt"
+
+    puts "Started testing for ''#{filename}''"
+    calc.load_data_from_file(filename)
+    test_data = calc.data
+    # puts "Data from file: '#{test_data}"
+
+    xs = []
+    ys = []
+
+    test_data.each do |point|
+      xs.push point[0]
+      ys.push point[1]
+    end
+    g = Gruff::Scatter.new(1200)
+    g.title = '5.txt data'
+    g.data(:points, xs, ys)
+    g.write("#{Dir.pwd}/test_data/5txt_graph.png")
+    calc.data
+    calc.build_by_kmeans_clustering
+    calc.find_all_optimal_k_indexes
+
+    Constants::Indexes.constants.map { |ce| Constants::Indexes.const_get(ce) }.each do |str|
+      calc.show_index_table(str)
+    end
+    calc.
+
+    calc.show_optimal_k_indexes_table
+    assert true
+  end
+end
